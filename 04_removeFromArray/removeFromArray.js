4e4f3a215d8250a0cec7dd89e71a811c78dc074e
danieltsega <danieltsega6658@gmail.com>
2024-08-07 23:24:31 +0300

    feat: add function that removes an element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+	let newArray = arr.filter(item => !args.includes(item));
+	return newArray;
 };
 
 // Do not edit below this line
