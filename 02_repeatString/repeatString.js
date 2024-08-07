345a0e1e0d70e3f38efdc23798e16c00bd8ffe5b
danieltsega <danieltsega6658@gmail.com>
2024-08-07 21:45:25 +0300

    feat: a function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+	let passedWord = '';
 
+	if (num < 0){
+		return "ERROR";
+	}
+	for (let i = 1; i <= num; i++){
+		passedWord += word;
+	}
+	return passedWord;
 };
 
 // Do not edit below this line
