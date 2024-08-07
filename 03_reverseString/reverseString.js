442470c967527185f5ac13934fffbc76ad99c075
danieltsega <danieltsega6658@gmail.com>
2024-08-07 21:56:37 +0300

    feat: add a function that reverses a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	let newWord = [...word];
+	let newArray = []
 
+	for (let i = newWord.length-1; i >= 0; i--){
+		newArray.push(newWord[i]);
+	}
+	return newArray.join('');
 };
 
 // Do not edit below this line
